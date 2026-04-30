d5547c8b8a7bb54f311ab7358fc37e82dfcd1f08
Duvan Verdun <duvanverdun@gmail.com>
2026-04-30 16:04:39 -0300

    refactor(rectangle): rectangles fully enabled and clearer readability - Remove wrong logic (rectangles cannot be squares) - Fixed docstring errors (rectangle and properties)

--- rectangle.py
+++ rectangle.py
@@ -1,21 +1,19 @@
 class Rectangle:
-    """Blueprint to create a parameter. Defining it by its measures (width and height) and being able to get its geometric properties (area and perimeter)."""
+    """Creates a rectangle defining its dimensions (width and height) and getting its geometric properties (area and perimeter)."""
     
     def __init__(self,width:int|float,height:int|float):
         if width <= 0 or height <= 0:
             raise ValueError(f"Width and height must be positive. Got '{width}','{height}'")
-        if width == height:
-            raise ValueError(f"Width and height of a rectangle cannot be the same. Got '{width}','{height}'")
         
         self._width = width
         self._height = height
         
     @property
     def area(self) -> float:
-        """Instance method to get the area of the rectangle"""
+        """Property that returns the area of the rectangle"""
         return self._width * self._height
     
     @property
     def perimeter(self) -> float:
-        """Instance method to get the perimeter of the rectangle"""
+        """Property that returns the perimeter of the rectangle"""
         return 2 * (self._width + self._height)
